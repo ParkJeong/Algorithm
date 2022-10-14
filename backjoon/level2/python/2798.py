a4d7a3af8ab0927988291d24f78fa2b916bbc1ff
teddy <jh.park@fadutec.com>
2022-10-14 12:43:48 +0900

    level2-2798(fail) itertools-combinations

--- backjoon/level2/python/2798.py
+++ backjoon/level2/python/2798.py
@@ -24,3 +24,20 @@ result = 0
 bfs(0, 0)
 
 print(result)
+
+
+
+# 순열 조합
+# combinations_with_replacement() 중복조합
+from itertools import combinations, combinations_with_replacement
+
+CARD_NUM, TARGET_NUM = map(int, input()).split()
+CARD_LIST = list(map(int, input().split()))
+sum = 0
+
+for candidate_card_list in combinations(CARD_LIST, 3):
+    temp_sum = sum(candidate_card_list)
+    if sum < temp_sum <= TARGET_NUM:
+        sum = temp_sum
+
+print(sum)
