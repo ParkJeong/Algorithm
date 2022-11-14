3cafb208eb8029c70bf2aaac12fa59e5f4550b32
kangtak6291@naver.com <jh.park@fadutec.com>
2022-11-14 12:56:18 +0900

    programmers level3-pick_up_item(in-progress2)

--- programmers/level3/pick_up_item.py
+++ programmers/level3/pick_up_item.py
@@ -16,6 +16,7 @@
 
 from collections import deque
 
+
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
     BOARD_WIDTH = 50
@@ -39,23 +40,58 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
 
     # #  직사각형을 모두 맵에 표시
     # #  각 행별로 가장 왼쪽과 가장 오른쪽 좌표만 놔두고 모두 삭제
+    is_first_row = True
+    last_row = (-1, -1, -1)
     for temp_y in range(BOARD_HEIGHT):
-        is_first = False
-        first_coordinate = (-1, -1)
-        last_coordinate = (-1, -1)
+        is_first = True
+        first_x = -1
+        last_x = -1
 
         # 각 행별로 가장 왼쪽과 가장 오른쪽 기억
         for temp_x in range(BOARD_WIDTH):
             if board[temp_y][temp_x] == 1:
                 if is_first:
-                    first_coordinate = temp_x, temp_y
-                    is_first = True
-                last_coordinate = temp_x, temp_y
+                    first_x = temp_x
+                    is_first = False
+                last_x = temp_x
 
+        # 수정해야함 22/11/14
+        # 둘레를 살려야함...
         # 각 행별로 가장 왼쪽과 가장 오른쪽 사이에 모든 곳 삭제
-
-    # 맵에 아이템 표시
+        if not is_first:
+            if is_first_row:
+                is_first_row = False
+            else:
+                last_row = (temp_y, first_x, last_x)
+                for remove_x in range(first_x + 1, last_x):
+                    board[temp_y][remove_x] = 0
+    # 마지막 줄 살리기
+    for temp_x in range(last_row[1], last_row[2] + 1):
+        board[last_row[0]][temp_x] = 1
 
     # 캐릭터의 위치에서 맵의 아이템 까지 최단거리 찾기 BFS
+    board[characterY][characterX] = 0
+    queue = deque()
+    queue.append((characterX, characterY))
 
-    return answer
+    DIRECTION_X = [1, 0, -1, 0]
+    DIRECTION_Y = [0, 1, -1, 0]
+    while True:
+        x, y = queue.popleft()
+        if x == itemX and y == itemY:
+            return board[itemY][itemX]
+
+        for move_x, move_y in zip(DIRECTION_X, DIRECTION_Y):
+            next_x = x + move_x
+            next_y = y + move_y
+            if 0 <= next_x < 50 and 0 <= next_y < 50 and board[next_y][next_x] == 1:
+                board[next_y][next_x] = board[y][x] + 1
+                queue.append((next_x, next_y))
+
+
+# print(solution([[1,1,5,7]], 1, 1, 4, 7))
+print(solution([[2,1,7,5],[6,4,10,10]], 3, 1, 7, 10))
+print(solution([[2,2,5,5],[1,3,6,4],[3,1,4,6]], 1, 4, 6, 3))
+print(solution([[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]], 1, 3, 7, 8))
+print(solution([[1,1,8,4],[2,2,4,9],[3,6,9,8],[6,3,7,7]], 9, 7, 6, 1))
+
